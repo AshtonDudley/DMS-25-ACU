22ec76d1bf794a42e47b1238cdb61d04fd69a5a5
Ashton Dudley <ADudley@dal.ca>
2024-11-15 12:56:04 -0400

    Added basic precharge sequence

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,12 +18,15 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "adc.h"
+#include "dma.h"
 #include "fdcan.h"
+#include "tim.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "app_main.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -87,9 +90,13 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_FDCAN1_Init();
+  MX_ADC1_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-
+  app_init();
+  app_main();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -97,13 +104,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-    
-    HAL_Delay(1000);
-    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
 
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-    
-    
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -129,7 +130,13 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 8;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV4;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -139,11 +146,11 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
---
+++ Core/User/app_main.c
@@ -0,0 +1,97 @@
+#include "stm32g0xx_hal.h"
+#include "main.h"
+#include "dma.h"
+
+// Defines
+#define ADC_BUFFER_LEN 3
+
+
+
+// External Handles 
+extern ADC_HandleTypeDef hadc1;
+extern TIM_HandleTypeDef htim2; 
+
+
+// ADC Buffer
+volatile uint16_t adc_buf[ADC_BUFFER_LEN]; // NOTE MOVE TO VCU STYLE BUFFER
+
+
+void simple_precharge(){
+    // NOTE FOR TEST CIRCUIT PIN7 = AIR+, PIN6 = AIR=
+    // BUF[0] = VBATT, BUF[1] = VTS
+    uint32_t vbatt, vts = 0;
+    
+    // Close AIR -
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+
+    // Check current voltage of the battery
+    vbatt = adc_buf[0];
+    
+    // Enable Pre-charge
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+
+    // Check Voltage or Timer
+    uint32_t startTime = HAL_GetTick();
+    while ((HAL_GetTick() - startTime) < 5000){
+        // 
+    }
+ 
+    // Close AIR +
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+
+    // Disable Pre-charge
+    HAL_Delay(100);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+}
+
+
+void app_init(){
+    HAL_TIM_Base_Start(&htim2);
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_buf, ADC_BUFFER_LEN);
+    return;
+}
+
+void app_main(){
+    uint32_t enablePrecharge = 0;
+    uint32_t TSStatus = 0;
+
+    uint32_t currentTime = 0;
+    uint32_t prevTime = 0;
+
+    while (1){
+        
+        currentTime = HAL_GetTick();
+        if (currentTime - prevTime > 500){
+            prevTime = currentTime;
+            HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+        }
+
+
+        if (enablePrecharge == 1){
+            simple_precharge();
+            TSStatus = 1;
+            enablePrecharge = 0;
+        }
+
+        if (1){
+            uint32_t vbatt = adc_buf[0]; 
+            uint32_t vts = adc_buf[1];
+            // Check of TS voltage difference is greater then 10%
+            if (abs(vbatt - vts) > 0.2 * vbatt){ 
+                // Disable contactors
+                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+                HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);  
+                TSStatus = 0;
+            }
+        }
+    }
+}
+
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc) {
+	(void)hadc;
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_1);
+    (void)hadc;
+}
